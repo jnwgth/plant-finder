034a16a58504d2ee03cf3e3d10a2c23b9864ae6c
joergn <joergn225@gmail.com>
2025-05-26 19:46:15 -0700

    Initial commit

---
+++ backend.py
@@ -0,0 +1,52 @@
+import google.generativeai as genai
+ 
+API_KEY = 'your_api_key'
+PROJECT_ID = 'your_project_id'
+ 
+# Back-End
+
+class PlantFinder:
+    # Model Configuration
+    model_config = {
+        "temperature": 0.5,  # 1.0
+        "top_p": 0.99,
+        "top_k": 0,
+        "max_output_tokens": 4096,
+    }
+
+    def __init__(self, api_key, project_id):
+        self.api_key = API_KEY
+        genai.configure(api_key=self.api_key)
+        self.project_id = PROJECT_ID
+        self.user_input = "test"
+        self.model = genai.GenerativeModel('gemini-1.5-flash',
+                                    generation_config=self.model_config)
+ 
+    def get_response(self, image_path):
+
+        with open(image_path, "rb") as img_file:
+            image_bytes = img_file.read()
+
+        prompt = "Identify this plant."
+
+        response = self.model.generate_content(
+                    [prompt, {"mime_type": "image/jpeg", "data": image_bytes}])
+        
+        if response._error:
+            print(f"Error: {response.error.message}")
+            return None
+        
+        return response.text
+ 
+if __name__ == "__main__":
+    plantfinder = PlantFinder(api_key=API_KEY, project_id=PROJECT_ID)
+    plant_info = plantfinder.get_response("plant.jpg")
+
+    if plant_info:
+        print("Plant Information:\n")
+    else:
+        print("Failed to retrieve plant information.")
+        plant_info = "No information available."
+
+    print(plant_info)
+
---
+++ main.py
@@ -0,0 +1,75 @@
+from PyQt6.QtWidgets import QApplication, QLineEdit, QPushButton, QMainWindow, QTextEdit
+from PyQt6.QtWidgets import QFileDialog
+from PyQt6.QtGui import QFont
+import sys
+from backend import PlantFinder
+import threading
+ 
+API_KEY = 'your_api_key'
+PROJECT_ID = 'your_project_id'
+ 
+# Front-End
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.plant_finder = PlantFinder(api_key=API_KEY,
+                               project_id=PROJECT_ID)  # need to instantiate only once
+ 
+        self.setWindowTitle('Plant Finder')
+        self.setMinimumSize(850, 500)  # size of the window 700 px width 500 px height
+ 
+        # Add text area(chat area) widget
+        self.info_area = QTextEdit(self)
+        self.info_area.setReadOnly(True)
+        self.info_area.setFont(QFont("Arial",18))
+        self.info_area.setGeometry(30, 30, 650, 400)  # x,y co-ordinates width and height
+ 
+        # Add input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setFont(QFont("Arial",18))
+        self.input_field.setGeometry(30, 450, 650, 30)
+        self.input_field.returnPressed.connect(self.send_message)
+ 
+        # Add Find Button widget
+        self.find_button = QPushButton("Find", self)
+        self.find_button.setGeometry(700, 450, 100, 30)
+        self.find_button.clicked.connect(self.send_message)
+
+        # Add File Selection Button
+        self.file_button = QPushButton("Select Image", self)
+        self.file_button.setGeometry(700, 400, 100, 30)
+        self.file_button.clicked.connect(self.open_file_dialog)
+
+        self.selected_file = None  # Store selected file path
+
+        self.show()
+
+
+    def open_file_dialog(self):
+        file_dialog = QFileDialog(self)
+        file_path, _ = file_dialog.getOpenFileName(
+            self, "Select Image File", "", "Images (*.png *.jpg *.jpeg *.webp *.bmp)"
+        )
+        if file_path:
+            self.selected_file = file_path
+            self.input_field.setText(file_path)
+
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        # append() is a method of QTextEdit()
+        self.info_area.append(f"<p style='color:#333333'>File: {user_input}</p>")
+        self.input_field.clear()
+
+        thread = threading.Thread(target=self.get_ai_response, args=(user_input, ))
+        thread.start()
+
+    def get_ai_response(self, user_input):
+        response = self.plant_finder.get_response(user_input)
+        self.info_area.append(f"<p style='color:#333333; background-color:#E9E9E9'>Plant Information: {response}</p>")
+        #self.info_area.verticalScrollBar().setValue(self.chat_area.verticalScrollBar().maximum())
+        #self.info_area.repaint()
+ 
+app = QApplication(sys.argv)
+main_window = MainWindow()
+sys.exit(app.exec())
+ 
